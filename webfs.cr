8c04019073f96d23b6e31cc9e346b20e4be2f55e
mwie <mwiegand@seibert-media.net>
2019-07-18 23:48:37 +0200

    upload poc

--- webfs.cr
+++ webfs.cr
@@ -2,21 +2,20 @@ require "http"
 require "http/server"
 require "ecr"
 
-p Dir["*"]
-
 server = HTTP::Server.new do |context|
   method = context.request.method
   if method == "GET"
     # GET
-    context.response.content_type = "text/plain"
+    context.response.content_type = "text/html"
     files = Dir["*"]
     context.response.print ECR.render("index.ecr")
   elsif method == "POST"
     # PUST
-    name = file = nil
+    name = nil
+    file = nil
     HTTP::FormData.parse(context.request) do |part|
       case part.name
-      when "name"
+      when "path"
         name = part.body.gets_to_end
       when "file"
         file = File.tempfile("upload") do |file|
@@ -30,7 +29,6 @@ server = HTTP::Server.new do |context|
     end
     context.response << file.path
   elsif method == "DELETE"
-    p context.request.path
   end
 end
 
