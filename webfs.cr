46e7b0a80d45abf09637311bc6c34ba1a40ae778
mwie <mwiegand@seibert-media.net>
2019-07-19 23:09:28 +0200

    upload to correct position

--- webfs.cr
+++ webfs.cr
@@ -7,6 +7,9 @@ class String
   def relative_to(root : String)
     self.to_s[root.size..-1]
   end
+
+  def absolute_in(root : String)
+  end
 end
 
 # format int as si
@@ -30,22 +33,27 @@ root = i ? ARGV[i + 1].gsub(/\/$/, nil) : "/"
 server = HTTP::Server.new do |context|
   context.response.content_type = "text/html"
   method = context.request.method
+  request_path = context.request.path.gsub(/\/$/, nil)
+  request_path_absolute = "#{root}#{request_path}"
   if method == "GET"
-    #
-    # GET
-    #
-    request_path = context.request.path.gsub(/\/$/, nil)
-    ## title
-    elements = request_path.split('/')
-    title_elements = elements.map_with_index do |element, i|
-      root + elements[0..i].join("/")
+    if File.directory? request_path_absolute
+      #
+      # GET
+      #
+      # # title
+      elements = request_path.split('/')
+      title_elements = elements.map_with_index do |element, i|
+        root + elements[0..i].join("/")
+      end
+      # # entries
+      entries = Dir["#{request_path_absolute}/*"].map { |entry| entry }
+      dirs = entries.select { |entry| File.directory? entry }.sort
+      files = (entries - dirs).sort
+      sorted_entries = dirs + files
+      context.response.print ECR.render("index.ecr")
+    else
+      context.response.print "file"
     end
-    ## entries
-    entries = Dir["#{root}#{request_path}/*"].map{|entry| entry}
-    dirs =  entries.select{|entry| File.directory? entry}.sort
-    files = (entries - dirs).sort
-    sorted_entries = dirs + files
-    context.response.print ECR.render("index.ecr")
   elsif method == "POST"
     #
     # POST
@@ -53,10 +61,14 @@ server = HTTP::Server.new do |context|
     name = nil
     file = nil
     HTTP::FormData.parse(context.request) do |part|
-      case part.name
-      when "path"
-        name = part.body.gets_to_end
-      when "file"
+      if part.name == "file"
+        # name
+        filename_header = part.headers["Content-Disposition"].split(';')
+          .find{|e| /^filename=/ =~ e.strip}
+        if filename_header 
+          name = filename_header.split("=")[1].gsub(/"/, nil)
+        end
+        # file
         file = File.tempfile("upload") do |file|
           IO.copy(part.body, file)
         end
@@ -66,6 +78,7 @@ server = HTTP::Server.new do |context|
       context.response.status = :bad_request
       next
     end
+    File.rename file.path, "#{request_path_absolute}/#{name}"
     context.response << file.path
   elsif method == "DELETE"
     #
